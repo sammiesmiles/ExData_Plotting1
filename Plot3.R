18ddbbe03173e78159c1c0ff52b122e70ba8fe33
Sammie Jia <sammiesmiles@gmail.com>
2014-05-08 00:40:19 +1200

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,19 @@
+# setwd('C:/Coursera/Exploratory Data Analysis')
+
+
+mydata <- read.table('household_power_consumption.txt',
+                     header=TRUE,
+                     sep=';',na.string='?')
+mydata$Date2 <- with(mydata,as.Date(Date,'%d/%m/%Y'))
+mydata2 <- subset(mydata,Date2%in%as.Date(c('01/02/2007','02/02/2007'),'%d/%m/%Y'))
+mydata2$DateTime <- with(mydata2,strptime(paste(Date,Time,sep=' '),'%d/%m/%Y %H:%M:%OS'))
+
+
+png('plot3.png',width=480,height=480)
+with(mydata2,plot(DateTime,Sub_metering_1,type='l',xlab='',
+                  ylab='Energy sub metering'))
+with(mydata2,lines(DateTime,Sub_metering_2,type='l',col='red'))
+with(mydata2,lines(DateTime,Sub_metering_3,type='l',col='blue'))
+legend('topright',c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
+       lty=1,col=c('black','red','blue'))
+dev.off()
